6c25f6bbd0cdbb2eb14f40b5d0511fd5f5b7363c
Georg Kromer <georgkromer@protonmail.com>
2020-06-24 17:41:00 +0200

    downloading a file works now -> you need to install fiel-saver.js (described in comments)

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 
 import { switchMap } from 'rxjs/operators';
 
 import { DataService } from '../data.service';
+import { UploadFileService } from 'src/app/_services/upload-file.service';
+import { Observable } from 'rxjs';
+
+import { saveAs } from 'file-saver';  
+// You need to install fiel-saver.js by running these 2 commands in your terminal: 
+//1) npm install file-saver --save 
+//2) npm install @types/file-saver --save-dev    
+// https://www.javascripting.com/view/filesaver-js
 
 @Component({
   selector: 'app-details',
@@ -12,8 +20,11 @@ import { DataService } from '../data.service';
 })
 export class DetailsComponent implements OnInit {
   user: any;
+  userPic: Observable<any>;
+  userId: string;
+  imageUrl;
 
-  constructor(private route: ActivatedRoute, private data: DataService) { }
+  constructor(private route: ActivatedRoute, private data: DataService, private uploadFileService: UploadFileService) { }
 
   ngOnInit() {
     this.route.params.pipe(switchMap(params => this.data.getUser(params['id'])))
@@ -21,7 +32,27 @@ export class DetailsComponent implements OnInit {
         data => {
           this.user = data;
           console.log(this.user);
+          alert("Helloooooo" + data.id)
+         // this.userId = data.id.toString();
+          this.uploadFileService.getUserPic().subscribe(
+            data => {
+              // var urlCreator = window.URL || window.webkitURL;
+              // console.log("data: " + data);
+              
+              // alert(this.data);
+              // this.imageUrl = urlCreator.createObjectURL(data);
+              // console.log("blob: " + this.imageUrl);
+              
+              //document.querySelector("#image") = imageUrl;
+              saveAs(data, "test.png")
+            },
+            err => console.error(err)
+            );
         }
       );
   }
+
+  response(e) {
+    
+ }
 }
