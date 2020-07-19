c4dcc9336a97ed9196906788bbd9e74600ad4e5b
Georg <georgkromer@pm.me>
2020-07-19 11:47:38 +0200

    checking price input for new product works

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
-import { Product } from '../_models/product';
 
 
 @Component({
@@ -17,22 +16,28 @@ export class AddProductComponent implements OnInit {
     private dataService: DataService) { }
 
   addForm: FormGroup;
-
-  data: Product;
+  onlyNumbersMessage: string = "Bitte keinen Text oder negative Zahlen als Preis eingeben. 😉😉"
+  showOnlyNumbersMessage: boolean = false;
 
   ngOnInit(): void {
-    this.addForm = this.formBuilder.group({
-      userId: ['', Validators.required],
+    this.addForm = this.formBuilder.group({            //https://angular.io/guide/reactive-forms
       name: ['', Validators.required],
       title: ['', Validators.required],
-      price: ['', Validators.required],
-      views: ['', Validators.required],
+      price: ['0'],
       available: ['', Validators.required],
-      createdAt: ['', Validators.required],
-      updatedAt: ['', Validators.required],
       picPaths: ['', Validators.required]
+    }); 
+  }
 
-    });
+  // for invalid characters (text, negative numbers...), prevent input
+  inputValidator(event: any) {
+    // https://stackoverflow.com/questions/37435529/proper-way-to-restrict-text-input-values-e-g-only-numbers?noredirect=1&lq=1
+    if (event.target.value == '') {
+      this.addForm.get('price').patchValue('');
+      this.showOnlyNumbersMessage = true;
+    } else {
+      this.showOnlyNumbersMessage = false;
+    }
   }
 
   onSubmit() {
@@ -42,14 +47,15 @@ export class AddProductComponent implements OnInit {
          console.log('Product created successfully!')
          this.router.navigate(['products']);
        }); */
-       
+
     console.log(this.addForm.value);
 
       var formData: any = new FormData();
       formData.append("name", this.addForm.get('name').value);
       formData.append("title", this.addForm.get('title').value);
+      
       formData.append("price", this.addForm.get('price').value);
-    
+   
     this.dataService.addProduct(formData)
       .subscribe((res: any) => {
         console.log(res);
