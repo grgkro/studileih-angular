e6d638192585d81be7f8c1abf69414e2af51361b
Georg <georgkromer@pm.me>
2020-08-08 11:47:06 +0200

    only showing users from the own dorm in users overview

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { trigger,style,transition,animate,query,stagger } from '@angular/animations';
 import { User } from '../_models/user';
-import { Observable, asyncScheduler } from 'rxjs';
+import { Observable, asyncScheduler, Subject } from 'rxjs';
 import { UpdateService } from '../_services/update.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users',
@@ -38,14 +39,31 @@ export class UsersComponent implements OnInit {
 
   users$: Observable<User[]>;
   users: User[];
+  currentUser: User;
+
+   // Angular takes care of unsubscribing from many observable subscriptions like those returned from the Http service or when using the async pipe. But the routeParam$ and the _update.currentShowUploadComponent needs to be unsubscribed by hand on ngDestroy. Otherwise, we risk a memory leak when the component is destroyed. https://malcoded.com/posts/angular-async-pipe/   https://www.digitalocean.com/community/tutorials/angular-takeuntil-rxjs-unsubscribe
+   destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private _data: DataService, private _update: UpdateService) { }
 
   ngOnInit(): void {
-    this.users$ = this._data.getUsers()   // we dont need to subscribe here, we only get the result of the Observable users$ in the HTML
+    this.users$ = this._data.getUsers();   // we dont need to subscribe here, we only get the result of the Observable users$ in the HTML
+    this.updateUser();
     // const task = () => this._update.currentUsers$;
     // asyncScheduler.schedule(task, 1000);
     // this.users$ = this._update.currentUsers$
+  }
+
+  ngOnDestroy() {            // Angular takes care of unsubscribing from many observable subscriptions like those returned from the Http service or when using the async pipe. But the routeParam$ and the _update.currentShowUploadComponent needs to be unsubscribed by hand on ngDestroy. Otherwise, we risk a memory leak when the component is destroyed. https://malcoded.com/posts/angular-async-pipe/   https://www.digitalocean.com/community/tutorials/angular-takeuntil-rxjs-unsubscribe
+    this.destroy$.next(true);
+    // Now let's also unsubscribe from the subject itself:
+    this.destroy$.unsubscribe();
+  }
+
+  updateUser(): void {
+    this._update.currentUser
+    .pipe(takeUntil(this.destroy$))
+    .subscribe(user => this.currentUser = user)
   }
 
 }
