ae0e955c7c0fe86509bef8851b984544d48f319d
Georg <georgkromer@pm.me>
2020-08-26 21:39:11 +0200

    fixed sidebar

--- src/app/_services/application-state-service.service.ts
+++ src/app/_services/application-state-service.service.ts
@@ -8,7 +8,7 @@ export class ApplicationStateService {
   private isMobileResolution: boolean;
 
   constructor() {
-    if (window.innerWidth < 768) {
+    if (window.innerWidth < 300) {
       this.isMobileResolution = true;
     } else {
       this.isMobileResolution = false;
