9f2817f1f7fc67870c709f89d7d9091f00e4057e
Georg <georgkromer@pm.me>
2020-09-01 11:54:57 +0200

    fixed jasmine tests in User login component

--- src/app/_components/user-components/user-login/user-login.component.spec.ts
+++ src/app/_components/user-components/user-login/user-login.component.spec.ts
@@ -4,6 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing';
 import { TokenStorageService } from 'src/app/_services/token-storage.service';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
+import { of } from 'rxjs/internal/observable/of';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('UserLoginComponent', () => {
   let httpTestingController: HttpTestingController;
@@ -14,10 +16,20 @@ describe('UserLoginComponent', () => {
   // let tokenStorageService;
   // let authenticationService;
 
+  // https://stackoverflow.com/questions/63533754/angular-unit-testing-with-jasmine-error-please-add-an-ngmodule-annotation/63675374#63675374
+  tokenStorageServiceStub = {
+    saveToken: () => {}
+  };
+  
+  
+  authenticationServiceStub = {
+    login: () => of({status: 200, body: {}})
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [{HttpClientTestingModule}],
-      providers: [{provide: ReactiveFormsModule}, { provide: TokenStorageService, useValue: tokenStorageServiceStub }, { provide: AuthenticationService, useValue: authenticationServiceStub } ],
+      imports: [HttpClientTestingModule, ReactiveFormsModule, MatSnackBarModule],
+      providers: [{ provide: TokenStorageService, useValue: tokenStorageServiceStub }, { provide: AuthenticationService, useValue: authenticationServiceStub } ],
       declarations: [ UserLoginComponent ]
     })
 
@@ -33,4 +45,20 @@ describe('UserLoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be invalid when empty', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+  })
+
+  it('should check the username input validity', () => {
+    let username = component.loginForm.controls['username'];
+    expect(username.valid).toBeFalsy();
+  })
+
+  it('should check the password input validity', () => {
+    let password = component.loginForm.controls['password'];
+    expect(password.valid).toBeFalsy();
+  })
+
+ 
 });
