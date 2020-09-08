6977c335c7c709bbcf329f80d9dff50bd83e73a5
Georg <georgkromer@pm.me>
2020-09-08 14:17:46 +0200

    resizing images responsevily works

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -5,8 +5,8 @@ import { Observable } from 'rxjs';
 import { DataService } from '../data.service';
 
 // const serverPath = 'https://studileih-heroku.herokuapp.com';
-// const serverPath = 'http://localhost:8090';
-const serverPath = 'https://studileih1.eu-central-1.elasticbeanstalk.com';
+const serverPath = 'http://localhost:5000';
+// const serverPath = 'https://studileih1.eu-central-1.elasticbeanstalk.com';
 
 @Injectable({
   providedIn: 'root'
