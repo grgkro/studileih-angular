37e24840348ca662bdc728dd6bd22849c343b049
Georg <georgkromer@pm.me>
2020-09-16 15:59:08 +0200

    solved problem with additional navbar + deleted extra navbar with state difference

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -75,58 +75,6 @@ const routes: Routes = [
   { path: '**', redirectTo: '', pathMatch: 'full' } // = wildcard route: A well-functioning application should gracefully handle when users attempt to navigate to a part of your application that does not exist. To add this functionality to your application, you set up a wildcard route. The Angular router selects this route any time the requested URL doesn't match any router paths. Common choices include an application-specific PageNotFoundComponent, which you can define to display a 404 page to your users; or a redirect to your application's main component. A wildcard route is the last route because it matches any URL.
 ];
 
-const mobile_routes: Routes = [
-  {
-    path: 'add-product',
-    component: AddProductComponent
-  },
-  {
-    path: 'product-details/:id',
-    component: ProductDetailsComponent
-  },
-
-  {
-    path: 'users',
-    component: UsersComponent
-  },
-  {
-    path: 'add-user',
-    component: AddUserComponent
-  },
-  {
-    path: 'edit-user/:id',
-    component: EditUserComponent
-  },
-  {
-    path: 'details/:id',
-    component: DetailsComponent
-  },
-  {
-    path: 'user-login',
-    component: LoginOverviewComponent
-  },
-  {
-    path: 'messages',
-    component: MessagesComponent
-  },
-  {
-    path: 'chat/:id',
-    component: ChatComponent
-  },
-  {
-    path: 'upload-userPic',
-    component: UploadFileComponent
-  },
-  {
-    path: 'google-maps',
-    component: GoogleMapsComponent
-  }, 
-  { 
-    path: '', component: ProductsComponent, resolve: { products: ProductResolverService } 
- },
-  { path: '**', redirectTo: '', pathMatch: 'full' } // = wildcard route: A well-functioning application should gracefully handle when users attempt to navigate to a part of your application that does not exist. To add this functionality to your application, you set up a wildcard route. The Angular router selects this route any time the requested URL doesn't match any router paths. Common choices include an application-specific PageNotFoundComponent, which you can define to display a 404 page to your users; or a redirect to your application's main component. A wildcard route is the last route because it matches any URL.
-];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes), FormsModule, ReactiveFormsModule],
   exports: [RouterModule]
